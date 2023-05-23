361a728585676db70b5853c15387c7cdaf10425c
Renkel <guirenkel@gmail.com>
2023-05-23 15:39:30 -0300

    fix: fix configs path on sequelize.js

--- database/sequelize.js
+++ database/sequelize.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 const env = process.env.DEFINES || 'dev'
-const configs = require('config.json')[env]
+const configs = require('./config.json')[env]
 
 const sequelize = new Sequelize(configs.database, configs.username, configs.password, configs);
 
