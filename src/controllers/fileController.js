f291b770168b808197d982c82f270cf8c468fdaa
Renkel <guirenkel@gmail.com>
2023-05-23 15:55:18 -0300

    feat: creates users on database from a csv file

--- src/controllers/fileController.js
+++ src/controllers/fileController.js
@@ -1,10 +1,18 @@
+const UsersCSVHandler = require("../utils/usersCSVHandler")
+const User = require('../models/user')
+const HttpStatusCode = require("../constants/HttpStatusCode")
 module.exports = {
   create: async (req, res) => {
     try {
-      const file = "this is a string";
-      res.send(file)
+
+      const uploadedCSVPath = './test/mocks/valid.csv' //req.filePath
+      const parsedUsers = await UsersCSVHandler.parseUsers(uploadedCSVPath)
+      const result = await User.createUsers(parsedUsers)
+
+      res.send(result)
+
     } catch (error) {
-      console.log(error)
+      res.status(HttpStatusCode.INTERNAL_ERROR).send({error})
     }
   }
 }
--- src/models/user.js
+++ src/models/user.js
@@ -1,21 +1,26 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    static associate(models) {
-      // define association here
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../../database/sequelize');
+class User extends Model {
+  static associate(models) {
+    // define association here
+  }
+
+  static async createUsers(data){
+    try {
+      const rows = await User.bulkCreate(data)
+      return rows
+    } catch (error) {
+      console.log('[LOG] - MODEL USER - Failed to create users: ', error)
+      throw new Error('Failed to create users')
     }
   }
-  User.init({
-    name: DataTypes.STRING,
-    city: DataTypes.STRING,
-    country: DataTypes.STRING,
-    favorite_sport: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
-  return User;
-};
+}
+
+User.init(
+  { name: DataTypes.STRING, city: DataTypes.STRING, country: DataTypes.STRING, favorite_sport: DataTypes.STRING },
+  { sequelize, modelName: 'User' }
+);
+
+module.exports = User
