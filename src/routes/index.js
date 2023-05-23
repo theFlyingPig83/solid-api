b856afc55ec9132dc3c7177487f330728506b7b0
Renkel <guirenkel@gmail.com>
2023-05-23 12:52:09 -0300

    feat: adds route for not found url

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const FilesRouter = require('./files')
-const UsersRouter = require('./users')
+const UsersRouter = require('./users');
+const HttpStatusCode = require('../Enums/HttpStatusCode');
 const router = express.Router();
 
 router.use("/files", FilesRouter);
 router.use("/users", UsersRouter);
+router.use('*', (req, res) => { 
+  res.status(HttpStatusCode.NOT_FOUND).send('Page not found!')
+})
 
 module.exports = router
