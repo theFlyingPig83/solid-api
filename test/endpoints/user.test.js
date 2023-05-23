24f893bf0386cf3de40551344079f41789339420
Renkel <guirenkel@gmail.com>
2023-05-23 12:43:14 -0300

    test: adds testing for user endpoint

---
+++ test/endpoints/user.test.js
@@ -0,0 +1,22 @@
+const chaiHttp = require('chai-http');
+const chai = require('chai')
+const server = require('../../server');
+const HttpStatusCode = require('../../src/Enums/HttpStatusCode');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiHttp);
+const expect = chai.expect;
+chai.use(chaiAsPromised).should();
+
+const ENDPOINT = '/api/users/'
+
+describe('/GET USERS', () => {
+  it('Should reach the API and return an array', done => {
+
+    chai.request(server)
+      .get(ENDPOINT)
+      .end((err, res) => {
+        expect(res).to.have.status(HttpStatusCode.SUCCESS);
+        done();
+      })
+  })
+});
