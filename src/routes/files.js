e6ed11db09a0579ac06477f841ce1a66073b98cd
Renkel <guirenkel@gmail.com>
2023-05-23 18:31:09 -0300

    refac: rename middleware files

--- src/routes/files.js
+++ src/routes/files.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const fileController = require('../controllers/fileController');
-const uploadMiddleware = require('../middlewares/uploadMiddleware');
-const checkPayloadMiddleware = require('../middlewares/checkPayloadMiddleware');
+const multerUploadMiddleware = require('../middlewares/multerUpload');
+const checkPayloadMiddleware = require('../middlewares/checkPayload');
 const router = express.Router();
 
-router.post("/", uploadMiddleware.single('file'), checkPayloadMiddleware, fileController.create);
+router.post("/", multerUploadMiddleware.single('file'), checkPayloadMiddleware, fileController.create);
 
 module.exports = router;
