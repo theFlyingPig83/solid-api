6a294092e4b29d68a71c8b4afaa421bebafb3617
Renkel <guirenkel@gmail.com>
2023-05-23 13:05:48 -0300

    feat: adds user model

---
+++ src/models/user.js
@@ -0,0 +1,10 @@
+class User {
+  constructor({ name, city, country, favoriteSport }) {
+    this.name = name
+    this.city = city
+    this.country = country
+    this.favoriteSport = favoriteSport
+  }
+}
+
+module.exports = User
