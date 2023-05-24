22163c5164717d552b45d6370f6b0d23e435f2de
Renkel <guirenkel@gmail.com>
2023-05-24 13:04:56 -0300

    test: adds test coverage for fileController

---
+++ test/controllers/fileController.test.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const chai = require('chai');
+const sinon = require('sinon');
+const { expect } = chai;
+const controller = require('../../src/controllers/fileController');
+const UsersCSVHandler = require('../../src/utils/usersCSVHandler');
+const User = require('../../src/models/user');
+const FileErrors = require('../../src/constants/FileErrors');
+const mocks = {
+  validParsedUsers: require('../mocks/valid-parsed-users'),
+  validUserList: require('../mocks/valid-user-list')
+}
+describe('File Controller - Suite Tests', () => {
+  let res = {}
+  let next = {};
+  const req = {
+    file: {
+      path: '/path/to/uploaded.csv',
+    },
+    headers: {
+      session_id: '123456',
+    },
+  };
+
+  beforeEach(()=>{
+    res = { send: sinon.stub() };
+    next = sinon.stub()
+  })
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('should create users from uploaded CSV and return the result', async () => {
+    
+    const usersCSVHandlerStub = sinon.stub(UsersCSVHandler, 'parseUsers').resolves(mocks.validParsedUsers)
+    const next = sinon.stub();
+    const createUsersStub = sinon.stub(User, 'createUsers').resolves(mocks.validUserList);
+
+    await controller.create(req, res, next);
+
+    expect(createUsersStub.calledOnceWith(mocks.validParsedUsers, req.headers.session_id)).to.be.true;
+    expect(usersCSVHandlerStub.calledOnceWith(req.file.path)).to.be.true;
+    expect(res.send.calledOnceWith(mocks.validUserList)).to.be.true;
+    expect(next.notCalled).to.be.true;
+  });
+
+  it('should call next with error when an exception occurs', async () => {
+    
+    const errorMessage = FileErrors.FILE_FIELDS_ERROR_MESSAGE;
+    const parseUsersStub = sinon.stub(UsersCSVHandler, 'parseUsers').throws(new Error(errorMessage));
+
+    await controller.create(req, res, next);
+
+    expect(parseUsersStub.calledOnceWith(req.file.path)).to.be.true;
+    expect(res.send.notCalled).to.be.true;
+    expect(next.calledOnceWith(sinon.match.instanceOf(Error))).to.be.true;
+  });
+
+  it('should call next with error when User.createUsers throws an error', async () => {
+
+    sinon.stub(UsersCSVHandler, 'parseUsers').resolves(mocks.validParsedUsers)
+    const createUsersStub = sinon.stub(User, 'createUsers').throws(new Error('MOCK ERROR'));
+    await controller.create(req, res, next);
+    expect(createUsersStub.calledOnceWith(mocks.validParsedUsers, req.headers.session_id)).to.be.true;
+    expect(res.send.notCalled).to.be.true;
+
+  });
+});
---
+++ test/mocks/valid-user-list.js
@@ -0,0 +1,64 @@
+module.exports = [
+  [
+    {
+      "id": 19,
+      "session_id": "7d808b21-9f9f-499a-a19c-09ea47d6f42f",
+      "name": "John Doe",
+      "city": "New York",
+      "country": "USA",
+      "favorite_sport": "Basketball",
+      "createdAt": "2023-05-23T22:57:09.443Z",
+      "updatedAt": "2023-05-23T22:57:09.443Z"
+    },
+    {
+      "id": 20,
+      "session_id": "7d808b21-9f9f-499a-a19c-09ea47d6f42f",
+      "name": "Jane Smith",
+      "city": "London",
+      "country": "UK",
+      "favorite_sport": "Football",
+      "createdAt": "2023-05-23T22:57:09.443Z",
+      "updatedAt": "2023-05-23T22:57:09.443Z",
+    },
+    {
+      "id": 21,
+      "session_id": "7d808b21-9f9f-499a-a19c-09ea47d6f42f",
+      "name": "Mike Johnson",
+      "city": "Paris",
+      "country": "France",
+      "favorite_sport": "Tennis",
+      "createdAt": "2023-05-23T22:57:09.443Z",
+      "updatedAt": "2023-05-23T22:57:09.443Z"
+    },
+    {
+      "id": 22,
+      "session_id": "7d808b21-9f9f-499a-a19c-09ea47d6f42f",
+      "name": "Karen Lee",
+      "city": "Tokyo",
+      "country": "Japan",
+      "favorite_sport": "Swimming",
+      "createdAt": "2023-05-23T22:57:09.443Z",
+      "updatedAt": "2023-05-23T22:57:09.443Z"
+    },
+    {
+      "id": 23,
+      "session_id": "7d808b21-9f9f-499a-a19c-09ea47d6f42f",
+      "name": "Tom Brown",
+      "city": "Sydney",
+      "country": "Australia",
+      "favorite_sport": "Running",
+      "createdAt": "2023-05-23T22:57:09.443Z",
+      "updatedAt": "2023-05-23T22:57:09.443Z"
+    },
+    {
+      "id": 24,
+      "session_id": "7d808b21-9f9f-499a-a19c-09ea47d6f42f",
+      "name": "Emma Wilson",
+      "city": "Berlin",
+      "country": "Germany",
+      "favorite_sport": "Basketball",
+      "createdAt": "2023-05-23T22:57:09.443Z",
+      "updatedAt": "2023-05-23T22:57:09.443Z"
+    }
+  ]
+]
