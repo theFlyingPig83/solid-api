bd54935310d1ebc20840217c8fa99c980b8d1c55
Renkel <guirenkel@gmail.com>
2023-05-24 11:34:35 -0300

    refac: creates a new file error for sessions

---
+++ src/constants/SessionErrors.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MISSING_SESSION_ID_ERROR_MESSAGE:'Session identifier missing',
+  MISSING_SESSION_ID_UI_ERROR_MESSAGE:'It looks like you are not logged in!',
+}
--- src/middlewares/authChecker.js
+++ src/middlewares/authChecker.js
@@ -2,10 +2,11 @@ const path = require('path');
 const ApiError = require('../utils/ApiError');
 const HttpStatusCode = require('../constants/HttpStatusCode');
 const PayloadErrors = require('../constants/PayloadErrors');
+const SessionErrors = require('../constants/SessionErrors');
 
 function authChecker(req, res, next) {
   if(req.headers.session_id === undefined){
-    next(new ApiError(PayloadErrors.MISSING_SESSION_ID_ERROR_MESSAGE, HttpStatusCode.BAD_REQUEST, PayloadErrors.MISSING_SESSION_ID_UI_ERROR_MESSAGE, __filename))  
+    next(new ApiError(SessionErrors.MISSING_SESSION_ID_ERROR_MESSAGE, HttpStatusCode.BAD_REQUEST, SessionErrors.MISSING_SESSION_ID_UI_ERROR_MESSAGE, __filename))  
   }
   next()
 }
