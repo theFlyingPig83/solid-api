949bc3f5cc869518de31c06dfd0102df4a5f9abc
Renkel <guirenkel@gmail.com>
2023-05-24 09:46:40 -0300

    feat: adds CORS to project

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mainRouter = require('./src/routes/index');
 const { errorHandler } = require('./src/middlewares/errorHandler');
+const cors = require('cors');
 const server = express()
 const port = process.env.PORT || 5050
 
+server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use('/api', mainRouter)
