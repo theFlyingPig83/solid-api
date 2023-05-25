150197195c54b6823e38f5056548e1b531e80ca7
Renkel <guirenkel@gmail.com>
2023-05-25 10:34:46 -0300

    feat: adds file size validation on API

--- src/constants/FileErrors.js
+++ src/constants/FileErrors.js
@@ -5,6 +5,6 @@ module.exports = {
   FILE_FIELDS_ERROR_UI_MESSAGE: 'It looks like the CSV file is not properly formatted!',
   FILE_TYPE_NOT_ALLOWED: 'Only CSV files are allowed!',
   FILE_TYPE_NOT_ALLOWED_UI_MESSAGE: 'Hey! This file must be a CSV',
-  
-  
+  FILE_SIZE_TOO_LARGE: 'This file is too large! It must be smaller than 1MB!',
+  FILE_SIZE_TOO_LARGE_UI_MESSAGE: 'This file is too large! It must be smaller than 1MB!'
 }
--- src/middlewares/multerUpload.js
+++ src/middlewares/multerUpload.js
@@ -5,12 +5,14 @@ const HttpStatusCode = require('../constants/HttpStatusCode');
 const FileErrors = require('../constants/FileErrors');
 const fs = require('fs');
 
+const MAX_FILE_SIZE = 1024 * 1024; //1MB
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     const destination = path.resolve(__dirname, '..', '..', 'uploads')
     if (!fs.existsSync(destination)) {
       fs.mkdirSync(destination, { recursive: true })
-  }
+    }
     cb(null, destination);
   },
   filename: function (req, file, cb) {
@@ -19,9 +21,13 @@ const storage = multer.diskStorage({
 });
 
 const csvFilter = function (req, file, cb) {
+  const fileSize = parseInt(req.headers["content-length"])
   if (file.mimetype !== 'text/csv') {
     cb(new ApiError(FileErrors.FILE_TYPE_NOT_ALLOWED, HttpStatusCode.BAD_REQUEST, FileErrors.FILE_TYPE_NOT_ALLOWED_UI_MESSAGE, __filename), false);
-  } else {
+  } else if (fileSize > MAX_FILE_SIZE) {
+    cb(new ApiError(FileErrors.FILE_SIZE_TOO_LARGE, HttpStatusCode.BAD_REQUEST, FileErrors.FILE_SIZE_TOO_LARGE_UI_MESSAGE, __filename), false);
+  }
+  else {
     cb(null, true);
   }
 };
