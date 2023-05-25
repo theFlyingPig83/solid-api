aacb190cec4f45763e7e0b8ac2045ebecb074cef
Renkel <guirenkel@gmail.com>
2023-05-25 10:15:06 -0300

    refac: changes 404 route to server file

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mainRouter = require('./src/routes/index');
 const { errorHandler } = require('./src/middlewares/errorHandler');
 const cors = require('cors');
+const HttpStatusCode = require('./src/constants/HttpStatusCode');
+const { PAGE_NOT_FOUND } = require('./src/constants/ServerErrors');
 const server = express()
 const port = process.env.PORT || 5050
 
@@ -9,6 +11,11 @@ server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use('/api', mainRouter)
+
+server.use('*', (req, res) => {
+  res.status(HttpStatusCode.NOT_FOUND).send(PAGE_NOT_FOUND)
+})
+
 server.use(errorHandler)
 
 server.listen(port, () => {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,10 @@
 const express = require('express')
 const FilesRouter = require('./files')
 const UsersRouter = require('./users');
-const HttpStatusCode = require('../constants/HttpStatusCode');
 const sessionChecker = require('../middlewares/sessionChecker');
-const { PAGE_NOT_FOUND } = require('../constants/ServerErrors');
 const router = express.Router();
 
 router.use("/files", sessionChecker, FilesRouter);
 router.use("/users", sessionChecker, UsersRouter);
-router.use('*', (req, res) => {
-  res.status(HttpStatusCode.NOT_FOUND).send(PAGE_NOT_FOUND)
-})
 
 module.exports = router
