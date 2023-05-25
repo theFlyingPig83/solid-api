83b5289237eb12b0e7ba658e9d50214860038ac6
Renkel <guirenkel@gmail.com>
2023-05-25 09:05:39 -0300

    fix: use server error PAGE_NOT_FOUND instead of a string

--- src/constants/ServerErrors.js
+++ src/constants/ServerErrors.js
@@ -1,4 +1,5 @@
 module.exports = {
+  PAGE_NOT_FOUND: 'Page not found',
   INTERNAL_ERROR_MESSAGE: 'Internal Server Error',
   INTERNAL_ERROR_UI_MESSAGE: 'It looks like an error occurred on our end. Please try again in a few moments.',
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,13 @@ const FilesRouter = require('./files')
 const UsersRouter = require('./users');
 const HttpStatusCode = require('../constants/HttpStatusCode');
 const sessionChecker = require('../middlewares/sessionChecker');
+const { PAGE_NOT_FOUND } = require('../constants/ServerErrors');
 const router = express.Router();
 
 router.use("/files", sessionChecker, FilesRouter);
 router.use("/users", sessionChecker, UsersRouter);
 router.use('*', (req, res) => {
-  res.status(HttpStatusCode.NOT_FOUND).send('Page not found!')
+  res.status(HttpStatusCode.NOT_FOUND).send(PAGE_NOT_FOUND)
 })
 
 module.exports = router
